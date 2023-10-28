59b901aa16a9f3ec00d33572bce13ef7565376be
German Jofre <75284348+GermanJofre@users.noreply.github.com>
2023-10-28 18:53:28 -0300

    TP8 Finalizado.

--- src/main/java/com/crowdar/examples/steps/LoginSteps.java
+++ src/main/java/com/crowdar/examples/steps/LoginSteps.java
@@ -1,11 +1,8 @@
 package com.crowdar.examples.steps;
 
 import com.crowdar.core.PageSteps;
-import com.crowdar.core.actions.MobileActionManager;
-import com.crowdar.examples.constants.LoginConstants;
 import com.crowdar.examples.services.LoginService;
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class LoginSteps extends PageSteps {
